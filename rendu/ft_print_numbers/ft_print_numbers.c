ddfbcd98e9c72cf7917f44e139de8c95327c9549
louazzanimaryem <maryemlouazzani@gmail.com>
2025-07-31 00:09:22 +0100

    hello

--- rendu/ft_print_numbers/ft_print_numbers.c
+++ rendu/ft_print_numbers/ft_print_numbers.c
@@ -1,2 +1,13 @@
+include <unistd.h>
 
-pgregerhgethe
+void    ft_print_numbers(void)
+{
+	    char nb;
+
+	        nb = '0';
+		    while (nb <= '9')
+			        {
+					        write(1, &nb, 1);
+						        nb++;
+							    }
+}
